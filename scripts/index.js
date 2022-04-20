0548f725a5a781aad276695bb32148a9a87d9701
Evgeny Temirkaev <td-diana.ru@yandex.ru>
2022-04-20 12:11:43 +0300

    main

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,19 @@ let popupEdit = document.querySelector('.popup_edit');
 let profileButtonEdit = document.querySelector('.profile__button-edit');
 let popupCloseBtn = document.querySelectorAll('.popup__button-close');
 
-function popupOpen(popupObject) {
-    popupObject.classList.add('popup_opened');
-}
-
-profileButtonEdit.addEventListener('click', function (){
-    popupOpen(popupEdit);
-});
-
-function popupClose(popupObject){
-    popupObject.classList.remove(`popup_opened`);
-}
-
-popupCloseBtn.forEach(function (arrElement){
-    arrElement.addEventListener('click',  function(){
-        popupClose(this.closest('.popup'));
-    });
-});
-
-
 let popupForm = document.querySelector('.popup__form');
 let popupFieldName = popupForm.querySelector('.popup__field-name');
 let popupFieldAboutName = popupForm.querySelector('.popup__field-about-name');
 let profileName = document.querySelector('.profile__name');
 let profileAboutName = document.querySelector('.profile__about-name');
+
+function popupOpen(popupObject) {
+    popupObject.classList.add('popup_opened');
+}
+
+function popupClose(popupObject){
+    popupObject.classList.remove(`popup_opened`);
+}
 
 function popupFormSubmitHandler (evt) {
     evt.preventDefault();
@@ -33,5 +22,15 @@ function popupFormSubmitHandler (evt) {
     profileAboutName.textContent = popupFieldAboutName.value;
     popupClose(popupEdit);
 }
+
+profileButtonEdit.addEventListener('click', function (){
+    popupOpen(popupEdit);
+});
+
+popupCloseBtn.forEach(function (arrElement){
+    arrElement.addEventListener('click',  function(){
+        popupClose(this.closest('.popup'));
+    });
+});
 
 popupForm.addEventListener('submit', popupFormSubmitHandler);
